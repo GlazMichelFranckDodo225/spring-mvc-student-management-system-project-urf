2631896f4186015af880b65db3378d7548da9cca
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-21 07:30:16 +0200

    Added Validations to StudentDto Class which Acts as Model Object

--- src/main/java/com/dgmf/dto/StudentDto.java
+++ src/main/java/com/dgmf/dto/StudentDto.java
@@ -1,5 +1,7 @@
 package com.dgmf.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +12,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class StudentDto {
     private Long id;
+    @NotEmpty(message = "First Name Should Not Be Empty")
     private String firstName;
+    @NotEmpty(message = "Last Name Should Not Be Empty")
     private String lastName;
+    @NotEmpty(message = "Email Should Not Be Empty")
+    @Email
     private String email;
 }
